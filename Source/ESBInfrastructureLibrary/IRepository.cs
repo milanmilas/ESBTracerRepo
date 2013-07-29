8ab1b122b99f04cb678fc5f792324fc5bb76b1ff
Milan Milas <milanmilas@gmail.com>
2013-07-29 20:44:14 +0100

    repository

---
+++ Source/ESBInfrastructureLibrary/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBInfrastructureLibrary
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IQueryable<T> Fetch();
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Func<T, bool> predicate);
+        T Single(Func<T, bool> predicate);
+        T First(Func<T, bool> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void SaveChanges();        
+    }
+}
---
+++ Source/ESBTracerDataAccess/EFRepository.cs
@@ -0,0 +1,61 @@
+﻿using ESBInfrastructureLibrary;
+using ESBTracerDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBTracerDataAccess
+{
+    public class EFRepository<T> : IRepository<T>  where T : class
+    {
+        readonly DbContext _context;
+
+        public IQueryable<T> Fetch()
+        {
+            return _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Single(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T First(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
