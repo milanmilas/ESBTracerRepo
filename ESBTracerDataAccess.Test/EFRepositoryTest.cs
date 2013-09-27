9fc0706a27bd59cc535fef502b4930dc54d42d5d
milan.milas <milan.milas@gmail.com>
2013-09-27 15:40:02 +0100

    started testing, plus template black

---
+++ ESBTracerDataAccess.Test/EFRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ESBTracerDataAccess.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class EFRepositoryTest
+    {
+		
+    }
+}
